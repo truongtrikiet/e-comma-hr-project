fa09eb2b63cd199a7d9d7f950db01cbb05158c68
Kiet Truong <jadentruong2003@Jadens-Mac-Pro-M2.local>
2026-03-14 02:41:39 +0700

    fix: [bugfix/ECS-bugfix-1] fix login and notification

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -104,26 +104,39 @@
                             <div class="col-xl-12">
                                 <div class="auth-form">
                                     <h4 class="text-center mb-4">Sign in your account</h4>
+                                    <div class="d-flex justify-content-end mb-2">
+                                        <div>
+                                            <a href="{{ route('locale.switch', ['locale' => 'en', 'redirect_to' => route('auth.login.show-form')]) }}" class="mr-2">EN</a>
+                                            <a href="{{ route('locale.switch', ['locale' => 'vi', 'redirect_to' => route('auth.login.show-form')]) }}">VI</a>
+                                        </div>
+                                    </div>
+                                    @if ($errors->any())
+                                        <div class="alert alert-danger">
+                                            @foreach ($errors->all() as $error)
+                                                <div>{{ $error }}</div>
+                                            @endforeach
+                                        </div>
+                                    @endif
                                     <form method="POST" action="{{ route('auth.login') }}">
                                         @csrf
                                         <div class="form-group">
                                             <label><strong>Email</strong></label>
-                                            <input type="email" class="form-control" name="email">
+                                            <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}">
                                         </div>
                                         <div class="form-group">
                                             <label><strong>Password</strong></label>
-                                            <input type="password" class="form-control" name="password">
+                                            <input type="password" class="form-control @error('password') is-invalid @enderror" name="password">
                                         </div>
                                         <div class="form-row d-flex justify-content-between mt-4 mb-2">
                                             <div class="form-group">
                                                 <div class="form-check ml-2">
-                                                    <input class="form-check-input" type="checkbox" id="basic_checkbox_1">
+                                                    <input class="form-check-input" type="checkbox" id="basic_checkbox_1" name="remember" {{ old('remember') ? 'checked' : '' }}>
                                                     <label class="form-check-label" for="basic_checkbox_1">Remember me</label>
                                                 </div>
                                             </div>
-                                            <div class="form-group">
+                                            <!-- <div class="form-group">
                                                 <a href="{{ route('auth.reset.show-form') }}">Forgot Password?</a>
-                                            </div>
+                                            </div> -->
                                         </div>
                                         <div class="text-center">
                                             <button type="submit" class="btn btn-primary btn-block">Sign in</button>
